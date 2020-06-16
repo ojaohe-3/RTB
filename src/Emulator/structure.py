84536c333c39c1a8701e10e3f01bdfda213abc00
ojaohe-3 <ojaohe-3@student.ltu.se>
2020-06-16 17:09:00 +0200

    Added collision detection, fixed objects, now configuration is to be implemented. aswell as asyncio and aio_pika

--- src/Emulator/structure.py
+++ src/Emulator/structure.py
@@ -1,11 +1,33 @@
+import random
+import numpy as np
+
 class Structure:
     def __init__(self, pointMap):
-        self.points = pointMap
-        self.shape = self.boundsToShape()
+        if self.convexShapeTest(pointMap):
+            self.shape = pointMap
+        else:
+            self.shape = self.generateConvexStructure(random.randrange(0,8), 10*random.random())
 
-    def isInside(self, point):
-        return 0
+    def generateConvexStructure(self, edges,size):
+        return [[0,0]]
 
-    def boundsToShape(self):
-        #do somethings
-        return self.pointMap
+    def convexShapeTest(self,pointmap):
+        if pointmap is None:
+            return 0
+        else:
+            #for every vector of a shape in a point map, the angle between them must be less than 90 degrees
+            for i in range(0,len(pointmap)):
+                point = pointmap[i]
+                x1 = point[0]
+                y1 = point[1]
+
+                point[(i+1)%len(pointmap)]
+                x2 = point[0]
+                y2 = point[0]
+                x = x2-x1
+                y = y2 - y1
+
+                if np.abs(np.arctan(y/x)) > np.pie/2 :
+                    return 0
+
+        return 1

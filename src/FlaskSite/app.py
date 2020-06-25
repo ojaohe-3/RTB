1ec7efcdb232823e16675dfcbd8349346c6eb4aa
ojaohe-3 <ojaohe-3@student.ltu.se>
2020-06-25 14:38:59 +0200

    Emulator setup

--- src/FlaskSite/app.py
+++ src/FlaskSite/app.py
@@ -4,8 +4,25 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
+def index():
     return 'Hello World!'
+
+
+@app.route('/config', methods="POST")
+def postConfigs():
+    return ''
+
+
+@app.route('/data', methods="GET")
+def sendData():
+    # todo authentication
+    # todo get data from socket if available
+    # todo generate json and send to requester
+    return ''
+
+
+async def getData(conf, loop):
+    return ''
 
 
 if __name__ == '__main__':

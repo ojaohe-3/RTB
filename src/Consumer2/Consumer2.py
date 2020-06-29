6dcdf208e9a3c7bb4fd6ed28247a399bb06ba19c
André Christofferson <andrechristofferson@hotmail.com>
2020-06-29 14:42:52 +0200

    .

--- src/Consumer2/Consumer2.py
+++ src/Consumer2/Consumer2.py
@@ -43,7 +43,7 @@ class Consumer():
         self.config['rabbitmq']['sensor_exchange'], aio_pika.ExchangeType.DIRECT, durable=True
         )
 
-        self._queue = await self._channel.declare_queue("structures")
+        self._queue = await self._channel.declare_queue(self.config['rabbitmq']['queue'])
         await self._queue.bind(self._exchange)
         logger.info("connected to RMQ")
 
